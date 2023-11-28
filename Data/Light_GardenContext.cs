cf43700d88097d443c751c568fe3136124e7e3ec
PhamDoAnhKhoa <115057442+PhamDoAnhKhoa@users.noreply.github.com>
2023-11-28 19:37:57 +0700

    Add files via upload

---
+++ Data/Light_GardenContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using quan_ly_do_boi.Models;
+
+namespace Light_Garden.Data
+{
+    public class Light_GardenContext : DbContext
+    {
+        public Light_GardenContext (DbContextOptions<Light_GardenContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; } = default!;
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace quan_ly_do_boi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
